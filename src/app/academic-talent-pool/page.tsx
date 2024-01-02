6dd98a5e2031a275cf8c43d596d05ae13dc4a40e
Dan Grant <dan@zygadox.com>
2024-01-03 10:20:11 +1100

    Testing ESlint configs

--- src/app/academic-talent-pool/page.tsx
+++ src/app/academic-talent-pool/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         </h2>
         <p className="text-center">
           we want to create a community of academics who share similar passions,
-          interests an ideas. So that you have a space to connect, mtivate and
+          interests an ideas. So that you have a space to connect, motivate and
           ...
         </p>
         <p className="text-center">
