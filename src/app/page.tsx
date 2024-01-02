4f7607e501e7a778b0457133e4202d37fa5588ba
Dan Grant <dan@zygadox.com>
2024-01-02 15:31:52 +1100

    Update css

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,13 @@ import { FaBeer } from 'react-icons/fa';
 export default function Home() {
   return (
     <main className="container prose">
-      <h1 className="">Academic Jobs NextJS Frontend</h1>
+      <h1 className="bg-gray-600">Academic Jobs NextJS Frontend</h1>
       <h2 className="">Academic Jobs H2</h2>
       <h3 className="">Academic Jobs H3</h3>
       <h4 className="">Academic Jobs H4</h4>
       <h5 className="">Academic Jobs H5</h5>
       <h6 className="">Academic Jobs H6</h6>
-      <div className="text">
+      <div className="text mt-12">
         <p>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit. Vero
           laudantium natus deserunt voluptas assumenda doloremque libero quod
