9c64352b2979faaa5682d6fd085c140d8c8c384c
Dan Grant <dan@zygadox.com>
2024-01-04 11:59:21 +1100

    Add cloudinary

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['academicjobs.s3.amazonaws.com'],
+    domains: ['academicjobs.s3.amazonaws.com', 'res.cloudinary.com'],
   },
 };
 
