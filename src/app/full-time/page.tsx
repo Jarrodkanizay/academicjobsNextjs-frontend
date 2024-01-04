544c5eb3f59493cf9f88c02f92be75aecaf8c35c
SeanAcademicJobs <sean@academicjobs.com>
2024-01-04 16:26:05 +1100

    rest of category pages

---
+++ src/app/full-time/page.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  // title: 'About', //Option 1 replaces the %s in layout.tsx
+  title: {
+    absolute: '  Full-Time', //Option 2 overrides the title in layout.tsx
+  },
+  description:
+    'If you are looking for academic jobs online, you can browse through our job board to find the latest opportunities in this flexible and rewarding field.   ',
+  keywords: 'Academic Jobs Online. online positions',
+};
+export default function myPage() {
+  return (
+    
+      <main className="content-grid">
+              <h1>
+              Academic Full-Time Jobs
+              </h1>
+              <p>
+              Are you ready to take the next leap in your career journey? We are actively seeking a dedicated and passionate professional to join our dynamic team in a full-time role. As a key player in our organization, you will have the opportunity to contribute your expertise and make a significant impact. If you thrive in a collaborative environment, possess a strong work ethic, and are eager to take on new challenges, we invite you to apply. This is more than just a job – it's a chance to grow and excel in a supportive and innovative workplace. Join us on this exciting journey and let's build a successful future together. Your next career adventure awaits – apply now!</p>
+
+             
+      </main>
+   
+  );
+}
---
+++ src/app/graduate/page.tsx
@@ -0,0 +1,28 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  // title: 'About', //Option 1 replaces the %s in layout.tsx
+  title: {
+    absolute: '  Graduate Jobs', //Option 2 overrides the title in layout.tsx
+  },
+  description:
+    'Looking for a flexible and rewarding job ? Browse hundreds of graduate jobs in various roles and departments at your university. Apply today and gain valuable work experience and skills.',
+  keywords: 'Graduate Jobs , Graduate academic Jobs',
+};
+export default function myPage() {
+  return (
+    
+      <main className="content-grid">
+              <h1>
+              Graduate Jobs
+              </h1>
+              <p>
+              All graduate job openings from the top universities are available on our site. Apply on our website for amazing graduate roles worldwide, since our academic institutions are always looking for the best graduate jobs.</p>
+
+              <p className='pt-4'>Graduate jobs are temporary or part-time positions in a variety of departments and functions in a university or research institute. These positions give graduates the chance to improve their academic and social networks, earn money, obtain useful work experience, and develop employability skills. Graduate positions can be found in a variety of fields, such as research, teaching, administration, customer service, events, marketing, IT, and libraries. You can locate graduate programs, part-time and casual work, and unique internship possibilities in Australia and throughout the world if you're interested in seeking graduate jobs. You can obtain important work experience and abilities that will benefit you in your future career by applying for these opportunities.</p>
+
+             
+      </main>
+   
+  );
+}
