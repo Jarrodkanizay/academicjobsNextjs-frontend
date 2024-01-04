297bf852a445787abdbdcf9c08486f4a91d5e34a
WalterJin2 <walterjin2@gmail.com>
2024-01-05 09:35:10 +1100

    a

--- next.config.js
+++ next.config.js
@@ -1,8 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['academicjobs.s3.amazonaws.com', 'res.cloudinary.com'],
+    domains: ['academicjobs.s3.amazonaws.com', 'res.cloudinary.com', 'iloveacademicjobs.com'],
   },
 };
-
 module.exports = nextConfig;
