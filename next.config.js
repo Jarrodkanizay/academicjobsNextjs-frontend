993952ca4655af59337d99ba3a642fb72ce813ec
Dan Grant <dan@zygadox.com>
2024-01-05 10:13:43 +1100

    Update styles and config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['academicjobs.s3.amazonaws.com', 'res.cloudinary.com'],
+    domains: [
+      'academicjobs.s3.amazonaws.com',
+      'res.cloudinary.com',
+      'iloveacademicjobs.com',
+    ],
     // remotePatterns: [
     //   {
     //     protocol: 'https',
