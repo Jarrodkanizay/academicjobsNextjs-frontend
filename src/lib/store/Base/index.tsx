63f434a1c1ba1d3bc27c18a622ff2718692e530c
WalterJin2 <walterjin2@gmail.com>
2024-01-05 21:40:22 +1100

    q

--- src/lib/store/Base/index.tsx
+++ src/lib/store/Base/index.tsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 const baseURL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3500/api'
-    : 'https://api1.sciencejobs.com.au/api';
+    : 'https://api2.sciencejobs.com.au/api';
 
 const BaseApi = axios.create({
   baseURL,
