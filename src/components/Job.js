5bf0115243a0faf195257c0451568a63e4405c50
WalterJin2 <walterjin2@gmail.com>
2024-01-05 19:25:26 +1100

    a

--- src/components/Job.js
+++ src/components/Job.js
@@ -10,64 +10,60 @@ const Job = ({ job }) => {
   const { employer_id, title, location, activation_date, expiration_date, how_to_apply, logo, company_name, featured, id } = job
   content = (
     <>
-   
-    <article className="media bg-white border border-gray-200 p-4 mb-4 rounded-xl shadow-lg" data-id="59973"
-      onClick={() => {
-        //window.history.pushState({}, "New Page Title", `/jobs/${title?.replace(/\W+/g, '-').toLowerCase()}/${id}/`);
-        const params = new URLSearchParams({
-          id: encodeURIComponent(id),
-        });
-        router.push(`/job?${params.toString()}`)
-      }}
-    >
-      
-          {featured ? (
-            <div className="badge badge-sm badge-featured bg-[#f4a10c] text-white absolute top-[-0.3rem] right-[-0.3rem] border-transparent items-end pt-1">Featured</div>
-          ) : (
-            ""
-          )}
-
-      <div className="flex items-center  mb-2">
-        <div className="w-20 h-20 mr-4">
-          {/* <Link to={`/employers/${company_name}/${employer_id}/`}> */}
-          <img src={logo || "/favicon.png"} alt={company_name} className="w-full h-full object-contain rounded-lg" />
-          {/* </Link> */}
-        </div>
-        <div className="flex-1">
-          <span className="block text-blue-500 text-xl font-bold leading-tight hover:underline  cursor-pointer">{title}</span>
-          {/* <div className="hidden-xs mt-2">
+      <article className="media bg-white border border-gray-200 p-4 mb-4 rounded-xl shadow-lg" data-id="59973"
+        onClick={() => {
+          //window.history.pushState({}, "New Page Title", `/jobs/${title?.replace(/\W+/g, '-').toLowerCase()}/${id}/`);
+          const params = new URLSearchParams({
+            id: encodeURIComponent(id),
+          });
+          router.push(`/job?${params.toString()}`)
+        }}
+      >
+        {featured ? (
+          <div className="badge badge-sm badge-featured bg-[#f4a10c] text-white absolute top-[-0.3rem] right-[-0.3rem] border-transparent items-end pt-1">Featured</div>
+        ) : (
+          ""
+        )}
+        <div className="flex items-center  mb-2">
+          <div className="w-20 h-20 mr-4">
+            {/* <Link to={`/employers/${company_name}/${employer_id}/`}> */}
+            <img src={logo || "/favicon.png"} alt={company_name} className="w-full h-full object-contain rounded-lg" />
+            {/* </Link> */}
+          </div>
+          <div className="flex-1">
+            <span className="block text-blue-500 text-xl font-bold leading-tight hover:underline  cursor-pointer">{title}</span>
+            {/* <div className="hidden-xs mt-2">
             {featured ? <span className="inline-block bg-[#f4a10c] text-white px-2 py-1 text-xs font-bold rounded-full mr-2">Featured</span> : ''}
           </div> */}
-        </div>
-      </div>
-      <div className="flex justify-between items-center">
-        <div className="text-sm text-gray-600">
-          <div className="mb-1">{company_name}</div>
-          <div className="text-gray-700 font-light">{location}</div>
-        </div>
-        <div className="applications-close border-4 rounded p-1">
-              <p className="expiry-date-heading">Applications Close</p>
-              <p className="expiry-date">
-                  {expiration_date ?(
-                <time>
-                    {new Date(expiration_date).toLocaleDateString("en-US", {
-                      month: "short",
-                      day: "numeric",
-                      year: "numeric",
-                    })}
-                </time>
-                  ):(<p className='text-center'>NA</p>)}
-              </p>
-            </div>
-        {((new Date(expiration_date) < new Date()) && expiration_date) && (
-          <div className="bg-opacity-50 bg-red-500 text-white  text-sm px-1 py-1 rounded-full absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 rotate-[35deg] skew-y-0">
-            Job Fulfilled By AcademicJobs.com
           </div>
-        )}
-      </div>
-    </article>
-  </>
-
+        </div>
+        <div className="flex justify-between items-center">
+          <div className="text-sm text-gray-600">
+            <div className="mb-1">{company_name}</div>
+            <div className="text-gray-700 font-light">{location}</div>
+          </div>
+          <div className="applications-close border-4 rounded p-1">
+            <p className="expiry-date-heading">Applications Close</p>
+            <div className="expiry-date">
+              {expiration_date ? (
+                <time>
+                  {new Date(expiration_date).toLocaleDateString("en-US", {
+                    month: "short",
+                    day: "numeric",
+                    year: "numeric",
+                  })}
+                </time>
+              ) : (<p className='text-center'>NA</p>)}
+            </div>
+          </div>
+          {((new Date(expiration_date) < new Date()) && expiration_date) && (
+            <div className="bg-opacity-50 bg-red-500 text-white  text-sm px-1 py-1 rounded-full absolute top-[50%] left-[50%] transform -translate-x-1/2 -translate-y-1/2 rotate-[35deg] skew-y-0">
+              Job Fulfilled By AcademicJobs.com
+            </div>
+          )}
+        </div>
+      </article>
+    </>
   )
   return <div className='overflow-y w-full relative'>{content}</div>
 }
--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -59,7 +59,92 @@ export default function SearchResults({ q }: SearchResultsProps) {
       }
   };
   if (isFetching) {
-    // Your loading UI
+    content = (
+      <div className="container mx-auto flex w-full h-screen justify-between">
+        <div
+          role="status"
+          className="w-[40%] h-screen p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700"
+        >
+          <div className="flex items-center justify-between">
+            <div>
+              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            </div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            </div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            </div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            </div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            </div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          </div>
+          <span className="sr-only">Loading...</span>
+        </div>
+        <div
+          role="status"
+          className="w-[60%] h-screen p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700"
+        >
+          <div className="flex items-center justify-between">
+            <div>
+              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            </div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            </div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            </div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            </div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          </div>
+          <div className="flex items-center justify-between pt-4">
+            <div>
+              <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
+              <div className="w-32 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+            </div>
+            <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-700 w-12"></div>
+          </div>
+          <span className="sr-only">Loading...</span>
+        </div>
+      </div>
+    );
   } else if (isSuccess && data) {
     if (data.length > 0) {
       console.log('data', data);
