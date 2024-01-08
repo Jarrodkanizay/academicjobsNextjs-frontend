9f347e9b4cb50e00901efae7502ae6e876e1f9fa
Dan Grant <dan@zygadox.com>
2024-01-08 20:12:35 +1100

    Make baseURL any type

--- src/lib/store/Base/index.tsx
+++ src/lib/store/Base/index.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-const baseURL =
-  process.env.REACT_APP_ENV === 'https://api2.sciencejobs.com.au/api'
+const baseURL: any =
+  process.env.REACT_APP_ENV === 'https://api2.sciencejobs.com.au/api';
 
 const BaseApi = axios.create({
   baseURL,
