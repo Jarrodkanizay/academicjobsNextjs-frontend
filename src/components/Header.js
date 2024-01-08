fa1a9f0844fb6d33bb06a3aa286f61fc936d695b
Dan Grant <dan@zygadox.com>
2024-01-08 19:56:02 +1100

    Finalise nav on mobile

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,6 +7,7 @@ import LogoAJ from '@/components/brand/LogoAJ';
 import NavItem from '@/components/NavItem';
 import DispatchLink from '@/components/DispatchLink';
 import { useStore } from '@/lib/store/store';
+import HamburgerMenuIcon from '@/components/icons/HamburgerMenuIcon';
 
 import {
   countryMappings,
@@ -27,85 +28,95 @@ export default function Header() {
   };
 
   return (
-    <header className="">
-      <nav>
-        {pathname === '/' ? (
-          ''
-        ) : (
-          <Link href="/" className="static-logo mr-4">
-            <LogoAJ width={270} height={60} />{' '}
-          </Link>
-        )}
+    <>
+      <HamburgerMenuIcon
+        onClick={() => {
+          setIsNavOpen(!isNavOpen);
+          console.log('clicked');
+        }}
+      />
+      <header className={`header ${isNavOpen ? 'show-menu' : ''}`}>
+        <nav>
+          {pathname === '/' ? (
+            ''
+          ) : (
+            <Link href="/" className="static-logo mr-4">
+              <LogoAJ width={270} height={60} />{' '}
+            </Link>
+          )}
 
-        <NavItem
-          url="/jobs"
-          icon="/dotted-arrow.svg"
-          navLink="Seek Jobs"
-          forceClass="" //this is optional
-        />
-        <NavItem
-          url="/Global/employers"
-          icon="/upward-arrow.svg"
-          navLink="Top Universities"
-        />
-        <NavItem
-          url="/academic-hub"
-          icon="/network-icon.svg"
-          navLink="My Academic Hub"
-        />
-        <div
-          className={`hide-mobile p-2  md:w-52  dropdown-content z-[1] menu px-4 py-2   rounded-box w-31 } `}
-          style={{ maxWidth: '62px' }}
-          onMouseLeave={onMouseLeave}
-        >
-          <div className="  text-gray-900 relative" onMouseEnter={onMouseEnter}>
-            {countryMappings1[region].hasPic ? (
-              <Image
-                src={`https://academicjobs.s3.amazonaws.com/icon/countryFlag/${region}.svg`}
-                alt={region}
-                width={30}
-                height={30}
-              />
-            ) : (
-              <span className="" style={{ height: '20px' }}>
-                {region}
-              </span>
-            )}
-            <ul
-              onMouseLeave={onMouseLeave}
-              className={`${
-                dropdown ? 'block' : 'hidden'
-              } absolute  left-0 text-[0.875rem] z-99  min-w-[10rem] py-1 bg-white rounded-lg shadow-lg shadow-indigo-500/40`}
-              ref={ref}
+          <NavItem
+            url="/jobs"
+            icon="/dotted-arrow.svg"
+            navLink="Seek Jobs"
+            forceClass="" //this is optional
+          />
+          <NavItem
+            url="/Global/employers"
+            icon="/upward-arrow.svg"
+            navLink="Top Universities"
+          />
+          <NavItem
+            url="/academic-hub"
+            icon="/network-icon.svg"
+            navLink="My Academic Hub"
+          />
+          <div
+            className={`hide-mobile p-2  md:w-52  dropdown-content z-[1] menu px-4 py-2   rounded-box w-31 } `}
+            style={{ maxWidth: '62px' }}
+            onMouseLeave={onMouseLeave}
+          >
+            <div
+              className="  text-gray-900 relative"
+              onMouseEnter={onMouseEnter}
             >
-              {Object.keys(countryMappings1).map((key) => (
-                <li className="navbar__item" key={key}>
-                  <DispatchLink
-                    to={`/${key}/`}
-                    region={key}
-                    setDropdown={setDropdown}
-                    setIsNavOpen={setIsNavOpen}
-                    className="navbar__link"
-                  >
-                    {countryMappings1[key].menu}
-                  </DispatchLink>
-                </li>
-              ))}
-            </ul>
+              {countryMappings1[region].hasPic ? (
+                <Image
+                  src={`https://academicjobs.s3.amazonaws.com/icon/countryFlag/${region}.svg`}
+                  alt={region}
+                  width={30}
+                  height={30}
+                />
+              ) : (
+                <span className="" style={{ height: '20px' }}>
+                  {region}
+                </span>
+              )}
+              <ul
+                onMouseLeave={onMouseLeave}
+                className={`${
+                  dropdown ? 'block' : 'hidden'
+                } absolute  left-0 text-[0.875rem] z-99  min-w-[10rem] py-1 bg-white rounded-lg shadow-lg shadow-indigo-500/40`}
+                ref={ref}
+              >
+                {Object.keys(countryMappings1).map((key) => (
+                  <li className="navbar__item" key={key}>
+                    <DispatchLink
+                      to={`/${key}/`}
+                      region={key}
+                      setDropdown={setDropdown}
+                      setIsNavOpen={setIsNavOpen}
+                      className="navbar__link"
+                    >
+                      {countryMappings1[key].menu}
+                    </DispatchLink>
+                  </li>
+                ))}
+              </ul>
+            </div>
+            {/* </div> */}
+            {/* </div> */}
           </div>
-          {/* </div> */}
-          {/* </div> */}
-        </div>
 
-        <NavItem
-          url="/recruitment"
-          icon="/post-a-job-icon.svg"
-          navLink="Post a Job"
-          forceLinkClass="btn-mode"
-          forceButtonClass="nav-mobile-btn btn btn-aj"
-        />
-      </nav>
-      <style>{`
+          <NavItem
+            url="/recruitment"
+            icon="/post-a-job-icon.svg"
+            navLink="Post a Job"
+            forceLinkClass="btn-mode"
+            forceButtonClass="nav-mobile-btn btn btn-aj"
+          />
+        </nav>
+        <style>{`
       .hideMenuNav {
         display: none;
       }
@@ -125,6 +136,7 @@ export default function Header() {
         background: rgba(0,0,0,.4);
       }
     `}</style>
-    </header>
+      </header>
+    </>
   );
 }
