7dda3866b8f984c8e0a119b75d0eef496be1e121
Dan Grant <dan@zygadox.com>
2024-01-09 19:47:28 +1100

    Fix spelling mistakes

--- src/app/(employers)/employers/[name]/[id]/page.js
+++ src/app/(employers)/employers/[name]/[id]/page.js
@@ -1,5 +1,5 @@
 'use client';
-import React, { useState, useEffect, useRef } from 'react'
+import React, { useState, useEffect, useRef } from 'react';
 import { MdLocationPin } from 'react-icons/md';
 import { AiOutlineGlobal } from 'react-icons/ai';
 import SearchResults from '@/components/SearchResults';
@@ -7,30 +7,31 @@ import { useQuery } from '@tanstack/react-query';
 import axios from 'axios';
 import Image from 'next/image';
 import Link from 'next/link';
-import { BsFillShareFill } from "react-icons/bs";
-import { useParams } from 'next/navigation'
+import { BsFillShareFill } from 'react-icons/bs';
+import { useParams } from 'next/navigation';
 import BaseApi from '@/lib/store/Base';
 const Employer = () => {
-  const [viewJob, setViewJob] = useState(false)
-  const { id } = useParams()
-  console.log(id)
+  const [viewJob, setViewJob] = useState(false);
+  const { id } = useParams();
+  console.log(id);
   const { data, isSuccess, isLoading, isError, error } = useQuery({
-    queryKey: ["employer", id],
+    queryKey: ['employer', id],
     queryFn: async () => {
-      const response = await BaseApi.get(
-        `employer/${id}`,
-      );
+      const response = await BaseApi.get(`employer/${id}`);
       console.log(response.data);
       console.log('response.data.data', response.data.data);
       return response.data.data;
     },
     // enabled: id,
   });
-  let content
+  let content;
   if (isLoading) {
     content = (
-      <div className='flex  w-full h-screen  justify-between '>
-        <div role="status" className="w-[40%] h-screen p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700">
+      <div className="flex  w-full h-screen  justify-between ">
+        <div
+          role="status"
+          className="w-[40%] h-screen p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700"
+        >
           <div className="flex items-center justify-between">
             <div>
               <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
@@ -68,7 +69,10 @@ const Employer = () => {
           </div>
           <span className="sr-only">Loading...</span>
         </div>
-        <div role="status" className="w-[60%] h-screen p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700">
+        <div
+          role="status"
+          className="w-[60%] h-screen p-4 space-y-4 border border-gray-200 divide-y divide-gray-200 rounded shadow animate-pulse dark:divide-gray-700 md:p-6 dark:border-gray-700"
+        >
           <div className="flex items-center justify-between">
             <div>
               <div className="h-2.5 bg-gray-300 rounded-full dark:bg-gray-600 w-24 mb-2.5"></div>
@@ -107,91 +111,141 @@ const Employer = () => {
           <span className="sr-only">Loading...</span>
         </div>
       </div>
-    )
+    );
   } else if (isSuccess) {
-    console.log(data)
-    const { logo, company_name, website, company_description, location, Region, country } = data
-    console.log(company_description)
-    let location1 = '', company_description1 = ''
+    console.log(data);
+    const {
+      logo,
+      company_name,
+      website,
+      company_description,
+      location,
+      Region,
+      country,
+    } = data;
+    console.log(company_description);
+    let location1 = '',
+      company_description1 = '';
     if (company_description) {
-      company_description1 = company_description
+      company_description1 = company_description;
     } else {
-      if (Region) location1 = Region
-      if (country) location1 = country
+      if (Region) location1 = Region;
+      if (country) location1 = country;
       if (location1) {
-        company_description1 = `${company_name} is located at ${location1}.`
+        company_description1 = `${company_name} is located at ${location1}.`;
       } else {
-        company_description1 = `Welcome to ${company_name}. `
+        company_description1 = `Welcome to ${company_name}. `;
       }
       company_description1 = `${company_description1}<br><br>
-      For more infomation, join our Talent Pool`
-      if (website) company_description1 = `${company_description1}, or visit our university website at <a href=${website} style="text-decoration: underline; color: blue" >${website}</a>`
+      For more information, join our Talent Pool`;
+      if (website)
+        company_description1 = `${company_description1}, or visit our university website at <a href=${website} style="text-decoration: underline; color: blue" >${website}</a>`;
       company_description1 = `${company_description1}<br><br>
       To advertise a Job for ${company_name}, please <a href="/post-job/" style="text-decoration: underline; color: blue" >click here</a><br><br>
       To join the  ${company_name} Talent Pool, please <a href="#" style="text-decoration: underline; color: blue" >click here</a>
-      `
+      `;
     }
     content = (
-      <div className='flex-col gap-4 max-w-screen-xl mx-auto'>
-        <article className={`media ${company_name == 'Queensland University of Technology (QUT)' ? 'bg-blue-950' : 'bg-[#003663]'} border border-gray-300 p-4 mb-4 rounded-lg shadow-lg`}
+      <div className="flex-col gap-4 max-w-screen-xl mx-auto">
+        <article
+          className={`media ${
+            company_name == 'Queensland University of Technology (QUT)'
+              ? 'bg-blue-950'
+              : 'bg-[#003663]'
+          } border border-gray-300 p-4 mb-4 rounded-lg shadow-lg`}
         >
           <div className="md:flex md:justify-start .col-auto ">
             <div className="w-[8rem] h-[8rem] mr-4  ">
-              <img src={logo || "/favicon.png"}
+              <Image
+                src={logo || '/favicon.png'}
                 alt={company_name}
                 className="w-full h-full object-contain rounded-md bg-white "
                 onError={(e) => {
-                  e.target.src = 'https://academicjobs.s3.amazonaws.com/img/_misc/uni-profiles.png'
+                  e.target.src =
+                    'https://academicjobs.s3.amazonaws.com/img/_misc/uni-profiles.png';
                 }}
               />
             </div>
             <div className="md:flex md:flex-col gap-6 ">
-              <h1 className="text-3xl font-bold text-white pt-4">{company_name}</h1>
+              <h1 className="text-3xl font-bold text-white pt-4">
+                {company_name}
+              </h1>
               <div className="md:flex  md:gap-6 p-4">
                 <div className="text-white md:flex md:items-center md:justify-start md:gap-2 .col-auto p-4">
                   <MdLocationPin />
                   {location}
                 </div>
-                {
-                  website &&
+                {website && (
                   <div className="text-white md:flex md:items-center md:justify-start md:gap-2 p-4">
                     <AiOutlineGlobal />
-                    <a href={website} className="">{website}</a>
-                  </div>}
+                    <a href={website} className="">
+                      {website}
+                    </a>
+                  </div>
+                )}
               </div>
             </div>
           </div>
         </article>
         <div className="flex justify-start gap-4 mt-6 mb-2 pl-4">
-          <div className={` cursor-pointer text-xl text-orange-400    ${viewJob || 'underline'}`}
-            onClick={() => { setViewJob(false) }}
-          >Institution Description</div>
-          <div className={` cursor-pointer text-xl text-orange-400   ${viewJob && 'underline'}`}
-            onClick={() => { setViewJob(true) }}
-          >View Jobs</div>
-          <div className=' hidden cursor-pointer text-xl text-orange-400  '
+          <div
+            className={` cursor-pointer text-xl text-orange-400    ${
+              viewJob || 'underline'
+            }`}
+            onClick={() => {
+              setViewJob(false);
+            }}
+          >
+            Institution Description
+          </div>
+          <div
+            className={` cursor-pointer text-xl text-orange-400   ${
+              viewJob && 'underline'
+            }`}
+            onClick={() => {
+              setViewJob(true);
+            }}
+          >
+            View Jobs
+          </div>
+          <div
+            className=" hidden cursor-pointer text-xl text-orange-400  "
             onClick={(e) => {
               e.preventDefault();
               window.scrollTo({
-                top: document.querySelector("#my-target").offsetTop,
-                behavior: "smooth",
+                top: document.querySelector('#my-target').offsetTop,
+                behavior: 'smooth',
               });
-            }}>
+            }}
+          >
             View Jobs
           </div>
         </div>
-        <div className='max-w-screen-xl mx-auto'>
-          {
-            viewJob
-              ?
-              <SearchResults q={{ employer_id: id || 0 }} />
-              :
-              <div className={`max-h-50 overflow-y  border border-gray-300 p-4 mb-4 rounded-lg shadow-lg ${company_name == 'Bond University' && 'bg-[#f1b821]'} ${company_name == 'Queensland University of Technology (QUT)' && 'bg-blue-950'}`}>
-                <div className={`wrapper  ${company_name == 'Queensland University of Technology (QUT)' && 'text-white'}`}>
-                  <div dangerouslySetInnerHTML={{ __html: company_description1 }} />
-                  {company_name === 'Queensland University of Technology (QUT)' && (
-                    <style>
-                      {`
+        <div className="max-w-screen-xl mx-auto">
+          {viewJob ? (
+            <SearchResults q={{ employer_id: id || 0 }} />
+          ) : (
+            <div
+              className={`max-h-50 overflow-y  border border-gray-300 p-4 mb-4 rounded-lg shadow-lg ${
+                company_name == 'Bond University' && 'bg-[#f1b821]'
+              } ${
+                company_name == 'Queensland University of Technology (QUT)' &&
+                'bg-blue-950'
+              }`}
+            >
+              <div
+                className={`wrapper  ${
+                  company_name == 'Queensland University of Technology (QUT)' &&
+                  'text-white'
+                }`}
+              >
+                <div
+                  dangerouslySetInnerHTML={{ __html: company_description1 }}
+                />
+                {company_name ===
+                  'Queensland University of Technology (QUT)' && (
+                  <style>
+                    {`
         .wrapper span {
           color: white !important;
         }
@@ -199,11 +253,11 @@ const Employer = () => {
           color: yellow !important;
         }
       `}
-                    </style>
-                  )}
-                  {company_name === 'Bond University' && (
-                    <style>
-                      {`
+                  </style>
+                )}
+                {company_name === 'Bond University' && (
+                  <style>
+                    {`
         .wrapper span {
           color: white !important;
         }
@@ -211,17 +265,18 @@ const Employer = () => {
           color: white !important;
         }
       `}
-                    </style>
-                  )}
-                </div>
+                  </style>
+                )}
               </div>
-          } <div className="hidden" id="my-target">
-            <SearchResults id="about" q={{ employer_id: id || 0 }} /> </div>
+            </div>
+          )}{' '}
+          <div className="hidden" id="my-target">
+            <SearchResults id="about" q={{ employer_id: id || 0 }} />{' '}
+          </div>
         </div>
       </div>
-    )
+    );
   }
-  return <div className='overflow-y w-full'>
-    {content}</div>
-}
-export default Employer
+  return <div className="overflow-y w-full">{content}</div>;
+};
+export default Employer;
