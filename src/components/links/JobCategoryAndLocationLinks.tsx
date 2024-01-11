1488255177694f9ea9bfa1e4fa4152479b74e9e2
Dan Grant <dan@zygadox.com>
2024-01-11 14:18:56 +1100

    Job cat and location tweaks

--- src/components/links/JobCategoryAndLocationLinks.tsx
+++ src/components/links/JobCategoryAndLocationLinks.tsx
@@ -1,11 +1,15 @@
 import JobCategoryLinks from '@/components/links/JobCategoryLinks';
 import JobLocationTypeLinks from '@/components/links/JobLocationTypeLinks';
 
-const JobCategoryAndLocationLinks = () => {
+type Props = {
+  nativeTrue?: boolean;
+};
+
+const JobCategoryAndLocationLinks = ({ nativeTrue }: Props) => {
   return (
     <>
       <h2 className="underline-full mb-4">Done</h2>
-      <JobCategoryLinks />
+      <JobCategoryLinks nativeOn={nativeTrue} />
       <JobLocationTypeLinks />
     </>
   );
