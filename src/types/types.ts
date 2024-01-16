e8cd651724d3846bcb38a197b3d57bba17879ea9
Dan Grant <dan@zygadox.com>
2024-01-16 17:20:15 +1100

    Make types optional

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,17 +1,17 @@
 export type BlogPostTypes = {
   post: {
-    status: string;
-    title: string;
-    slug: string;
-    author: string;
-    published_at: string;
-    updated_at: string;
-    image_url: string;
-    alt: string;
-    summary: string;
-    tags: string[];
-    keywords: string;
-    content: string;
+    status?: string;
+    title?: string;
+    slug?: string;
+    author?: string;
+    published_at?: string;
+    updated_at?: string;
+    image_url?: string;
+    alt?: string;
+    summary?: string;
+    tags?: string[];
+    keywords?: string;
+    content?: string;
   };
-  columns: number;
+  columns?: number;
 };
