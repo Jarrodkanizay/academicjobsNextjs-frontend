41bd9d5f4ddafca5229ca29a4a80ae8f02888021
Dan Grant <dan@zygadox.com>
2024-01-16 11:49:27 +1100

    Add BlogPostProps type

--- src/components/blogs/BlogPost.tsx
+++ src/components/blogs/BlogPost.tsx
@@ -1,8 +1,18 @@
-'use client';
 import HeroBanner from '@/components/HeroBanner';
 
 type BlogPostProps = {
-  post: any;
+  status: string;
+  title: string;
+  slug?: string;
+  author?: string;
+  published_at?: string;
+  updated_at?: string;
+  image_url: string;
+  alt: string;
+  summary: string;
+  tags?: string[];
+  keywords: string;
+  content: string;
 };
 
 const BlogPost = ({ post }: BlogPostProps) => {
