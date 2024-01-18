6ad10de3faa586735fe7ee1a87fa2d39ba1e986e
WalterJin2 <walterjin2@gmail.com>
2024-01-19 10:05:42 +1100

    1

--- src/app/(features)/jobelephant/post-job/page.js
+++ src/app/(features)/jobelephant/post-job/page.js
@@ -6,6 +6,7 @@ import { keepPreviousData, useQuery, useMutation } from '@tanstack/react-query';
 import BaseApi from '@/lib/store/Base';
 import Link from 'next/link';
 const TalentPoolForm = () => {
+  let content
   const {
     register,
     handleSubmit,
@@ -23,7 +24,7 @@ const TalentPoolForm = () => {
     //e.preventDefault();
     //alert()
     console.log('data', data)
-    mutation.mutate({ ...data, "00_formSource":"Message from: Job Elephant Post Form"})
+    mutation.mutate({ ...data, "00_formSource": "Message from: Job Elephant Post Form" })
   }
   if (mutation.isLoading) {
     return (
@@ -40,60 +41,70 @@ const TalentPoolForm = () => {
     return <div>An error occurred: {mutation.error.message}</div>;
   }
   if (mutation.isSuccess) {
-    return <div>Email Sent!</div>
+    content = (
+    
+      <div className='h-[600px] text-xl font-bold flex flex-col justify-center items-center'>
+        
+        <div>Job Info Sent!</div>
+        <div>Pls make the payment by clicking the pay button below</div>
+      
+      </div>
+    )
+  } else {
+    content = (
+      <form className=" " onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex flex-col gap-4 justify-start">
+          <h1 className="text-4xl font-bold mb-6">
+            JobElephant Post Form
+          </h1>
+          <InputBlock register={register} errors={errors}
+            label="Name (Job Elephant)"
+            type="text"
+            field="01_Name_Job_Elephant"
+            forceClass=" py-3 text-black"
+            placeholder=""
+          />
+          <InputBlock register={register} errors={errors}
+            label="Email"
+            type="text"
+            field="02_Email"
+            forceClass=" py-3 text-black"
+            placeholder=""
+          />
+          <InputBlock register={register} errors={errors}
+            label="Job Elephant Client (ie: Utah University)"
+            type="text"
+            field="03_JobElephantClient"
+            forceClass=" py-3 text-black"
+            placeholder=""
+          />
+          <InputBlock register={register} errors={errors}
+            label="Job Link URL (ie: apptrkr.com/...)"
+            type="text"
+            field="04_Job_Link_URL"
+            forceClass=" py-3 text-black"
+            placeholder=""
+          />
+          <label className="font-semibold text-gray-400" htmlFor="notes">
+            Notes
+          </label>
+          <textarea className="bg-white text-black text-sm" id="notes" placeholder="Type your message here."
+            {...register("05_Notes")}
+          />
+          <button className="btn btn-accent mt-4">Submit</button>
+        </div>
+      </form>
+    )
   }
   return (
     <div className="max-w-md mx-auto my-10 flex flex-col gap-4 justify-start">
-     
-    <form className=" " onSubmit={handleSubmit(onSubmit)}>
-      <div className="flex flex-col gap-4 justify-start">
-        <h1 className="text-4xl font-bold mb-6">
-          JobElephant Post Form
-        </h1>
-        <InputBlock register={register} errors={errors}
-          label="Name (Job Elephant)"
-          type="text"
-          field="01_Name_Job_Elephant"
-          forceClass=" py-3 text-black"
-          placeholder=""
-        />
-        <InputBlock register={register} errors={errors}
-          label="Email"
-          type="text"
-            field="02_Email"
-          forceClass=" py-3 text-black"
-          placeholder=""
-        />
-        <InputBlock register={register} errors={errors}
-          label="Job Elephant Client (ie: Utah University)"
-          type="text"
-            field="03_JobElephantClient"
-          forceClass=" py-3 text-black"
-          placeholder=""
-        />
-        <InputBlock register={register} errors={errors}
-          label="Job Link URL (ie: apptrkr.com/...)"
-          type="text"
-            field="04_Job_Link_URL"
-          forceClass=" py-3 text-black"
-          placeholder=""
-        />
-        <label className="font-semibold text-gray-400" htmlFor="notes">
-          Notes
-        </label>
-        <textarea className="bg-white text-black text-sm" id="notes" placeholder="Type your message here."
-            {...register("05_Notes")}
-        />
-        <button className="btn btn-accent mt-4">Submit</button>
-      </div>
-      </form>
+      {content}
       <Link
         className="btn btn-prima mt-4"
         href="https://buy.stripe.com/test_cN2eYG56A0v12gU3cc"
       >
         Pay Now
       </Link>
-   
     </div>
   );
 };
