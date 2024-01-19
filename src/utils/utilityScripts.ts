776bad1fcb6b5d7b5fbc755701459b1252617fd8
WalterJin2 <walterjin2@gmail.com>
2024-01-19 12:32:57 +1100

    a

--- src/utils/utilityScripts.ts
+++
@@ -1,6 +0,0 @@
-export function formatDate(input: string) {
-  if (input === '') return '';
-  const date = new Date(input);
-  const options = { year: 'numeric', month: 'long', day: 'numeric' };
-  return date.toLocaleDateString('en-US', options);
-}
