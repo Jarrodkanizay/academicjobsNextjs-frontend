683a960bd2ecac2d3b7c56287fca6510b40d01de
Dan Grant <dan@zygadox.com>
2024-01-22 12:54:14 +1100

    Add hidden form fields

--- src/components/forms/InputBlock.js
+++ src/components/forms/InputBlock.js
@@ -1,4 +1,4 @@
-'use client'
+'use client';
 const InputBlock2 = ({
   ID,
   name,
@@ -12,11 +12,10 @@ const InputBlock2 = ({
   placeholder,
   tabIndex,
   required,
+  hidden,
 }) => {
   return (
-    <div
-      className={`w-full flex flex-col gap-1.5 items-start ${forceClass}`}
-    >
+    <div className={`w-full flex flex-col gap-1.5 items-start ${forceClass}`}>
       <label className="label-text text-xs">{label}</label>
       <input
         tabIndex={tabIndex}
@@ -25,6 +24,7 @@ const InputBlock2 = ({
         className="w-full input input-md input-bordered focus:outline-none focus:border-orange-500"
         autoComplete={autoComplete}
         placeholder={placeholder}
+        hidden={hidden}
       />
       {errors[field] && <span className="error">{errors[field].message}</span>}
     </div>
