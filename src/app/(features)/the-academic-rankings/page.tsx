2951ab648f511f300948b4ddc4ab92effe0f03e0
JamieRuskin <YOLOjarrod70>
2024-01-30 11:06:40 +1100

    page update

--- src/app/(features)/the-academic-rankings/page.tsx
+++ src/app/(features)/the-academic-rankings/page.tsx
@@ -93,10 +93,147 @@ export default function myPage() {
             </h2>
   
   
-  {/* Cards */}
-  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-8">
+ {/* Main Content */}
+ <section className="container mx-auto px-4 mt-12">
+          {/* <h2 className="text-center mb-6">
+            Need Cash? Fund your personal life with a Academicjob…
+          </h2>
+          <div className="flex flex-wrap items-center justify-center gap-3 mb-16">
+            <Link href={`/jobs/student-part-time`}>
+              <div className="badge badge-outline p-3">
+                Part-Time Academic Jobs
+              </div>
+            </Link>
+            <Link href={`/jobs/student-casual`}>
+              <div className="badge badge-outline p-3">Casual Academic Jobs</div>
+            </Link>
+            <Link href={`/jobs/student-full-time`}>
+              <div className="badge badge-outline p-3">
+                Full-Time Academic Jobs
+              </div>
+            </Link>
+            <Link href={`/student`}>
+              <div className="badge badge-outline p-3">
+                Find All Academic Jobs
+              </div>
+            </Link>
+          </div> */}
+          {/* Diverse Roles Card */}
+         
+         
+         {/* Gain Real-World Experience in Academic Settings Card */}
+         <div className="card card-side bg-slate-100 shadow-xl my-16 px-6 grid sm:grid-cols-1 md:grid-cols-2">
+            <figure>
+              <Image
+                width={2000}
+                height={534}
+                src="https://academicjobs.s3.amazonaws.com/img/banner/grow+yourself+globally.avif"
+                alt="Experience the Real-World in Academic Settings"
+                className="w-full lg:max-w-lg rounded-xl m-6"
+              />
+            </figure>
+            <div className="card-body w-full">
+              <h2 className="card-title">
+              GROW YOURSELF GLOBALLY
+              </h2>
+              <p className="max-w-screen-sm">
+              The academic field is one of the most competitive arenas to be involved in, both locally and globally. By establishing an individual rank for an academic, you can position yourself as a top professor in the field, or as an individual who can move up the ranks overtime. This helps academics further develop their career and/or legitimately establish themselves as a leader in academia through a globally recognised system.
+              </p>
+              <div className="card-actions justify-center md:justify-end">
+                <Link className="btn btn-aj" href="/student">
+                  Develop Your Network
+                </Link>
+              </div>
+            </div>
+          </div>
+         
+         
+          <div className="card card-side bg-slate-100 shadow-xl my-16 px-6 grid sm:grid-cols-1 md:grid-cols-2">
+          
+            <figure className="lg:order-1">
+              <Image
+                width={2000}
+                height={534}
+                src="https://academicjobs.s3.amazonaws.com/img/banner/collaboration+over+competition+.jpg"
+                alt="Choose from diverse campus roles"
+                className="w-full lg:max-w-lg rounded-xl m-6"
+              />
+           
+            </figure>
+             <div className="card-body w-full">
+              <h2 className="card-title">
+              COLLABORATION OVER COMPETITION
+              </h2>
+              <p className="max-w-screen-sm">
+              By providing a rank for you, we can set a benchmark that encourages collaboration. Having a number in the academic field allows you to find others with a similar rank, acknowledging a similar skill set, academic history, and research expertise. This knowledge can encourage collaborations on academic projects, expanding both members knowledge set and creativity.
+              </p>
+        
+              <div className="card-actions justify-center md:justify-end">
+                <Link className="btn btn-aj" href="/student">
+                Earn Extra Cash
+                </Link>
+              </div>
+            </div>
+          </div>
+          {/* Flexible Part-Time Opportunities for Academics Card */}
+          <div className="card card-side bg-sky-50 shadow-xl my-16 px-6 grid sm:grid-cols-1 md:grid-cols-2">
+            <figure>
+              <Image
+                width={2000}
+                height={534}
+                src="https://academicjobs.s3.amazonaws.com/img/banner/analyse+your+academic+competition.png"
+                alt="Work Part-Time with Flexible Opportunities for Academics"
+                className="w-full lg:max-w-lg rounded-xl m-6"
+              />
+            </figure>
+            <div className="card-body w-full">
+              <h2 className="card-title">
+              ANALYSE YOUR ACADEMIC PROGRESS
+              </h2>
+              <p className="max-w-screen-sm">
+              Academics are known to engage in large depths of analysis behind their journals, reports, and projects. A legitimate rank of the members of academia can allow for further analysis on the performance of modern-day academics and their improvement over the years. In turn, this analysis can describe positive trends on the future of academia and promote further success in the field.
+              </p>
+              <div className="card-actions justify-center md:justify-end">
+                <Link className="btn btn-aj" href="/student">
+                Search Academic Rankings
+                </Link>
+              </div>
+            </div>
+          </div>
+       
+       
+         {/* Build Your Network with On-Campus Employment Card */}
+         <div className="card card-side bg-sky-50 shadow-xl my-16 px-6 grid sm:grid-cols-1 md:grid-cols-2">
+            <figure className="lg:order-1">
+              <Image
+                width={2000}
+                height={534}
+                src="https://academicjobs.s3.amazonaws.com/img/banner/2+embrace+indiviudality+.png"
+                alt="Build Your Network with On-Campus Employment"
+                className="w-full lg:max-w-lg rounded-xl m-6"
+              />
+            </figure>
+            <div className="card-body w-full">
+              <h2 className="card-title">
+                EMBRACE YOUR INDIVIDUALITY
+              </h2>
+              <p className="max-w-screen-sm">
+              Set yourself apart from your university ranking. Academics often find themselves stuck in a partnership with their university. This may sometimes be great, but your true talent may go unrecognised under the universities rank which lends itself to the academic output and students results rather than your own work. Your own personal rank acts as an identifier of your success and establishment, putting you on the forefront as an academic, and not just a university professor.
+              </p>
+              <div className="card-actions justify-center md:justify-start">
+                <Link className="btn btn-aj" href="/student">
+                  Gain Valuable Skills 
+                </Link>
+              </div>
+            </div>
+          </div>
+       </section>
+
+
+  {/* Cards
+  <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-4 mt-8"> */}
             {/* Card 1 */}
-            <div className="card bg-slate-100 shadow-xl">
+            {/* <div className="card bg-slate-100 shadow-xl">
               <figure className="px-10 pt-10">
                 <div className="icon-ai">
                   <svg
@@ -129,10 +266,10 @@ export default function myPage() {
                   </span>
                 </p>
               </div>
-            </div>
+            </div> */}
 
             {/* Card 2 */}
-            <div className="card bg-slate-100 shadow-xl">
+            {/* <div className="card bg-slate-100 shadow-xl">
               <figure className="px-10 pt-10">
                 <div className="icon-ai">
                   <Image
@@ -155,10 +292,10 @@ export default function myPage() {
                   </span>
                 </p>
               </div>
-            </div>
+            </div> */}
 
             {/* Card 3 */}
-            <div className="card bg-slate-100 shadow-xl">
+            {/* <div className="card bg-slate-100 shadow-xl">
               <figure className="px-10 pt-10">
                 <div className="icon-ai">
                   <Image
@@ -177,11 +314,11 @@ export default function myPage() {
                 Academics are known to engage in large depths of analysis behind their journals, reports, and projects. A legitimate rank of the members of academia can allow for further analysis on the performance of modern-day academics and their improvement over the years. In turn, this analysis can describe positive trends on the future of academia and promote further success in the field.
                 </p>
               </div>
-            </div>
+            </div> */}
             {/* End Cards */}
           
             {/* Card 4 */}
-            <div className="card bg-slate-100 shadow-xl">
+            {/* <div className="card bg-slate-100 shadow-xl">
               <figure className="px-10 pt-10">
                 <div className="icon-ai">
                   <Image
@@ -201,9 +338,9 @@ export default function myPage() {
 
                 </p>
               </div>
-            </div>
+            </div> */}
             {/* End Cards */}
-          </div>
+          {/* </div> */}
 
           <div
           className="hero h-screen max-h-[480px] md:max-h-[400px]"
@@ -235,93 +372,13 @@ export default function myPage() {
        
        
        
-        {/* Main Content */}
-        <section className="container mx-auto px-4 mt-12">
-          {/* <h2 className="text-center mb-6">
-            Need Cash? Fund your personal life with a Academicjob…
-          </h2>
-          <div className="flex flex-wrap items-center justify-center gap-3 mb-16">
-            <Link href={`/jobs/student-part-time`}>
-              <div className="badge badge-outline p-3">
-                Part-Time Academic Jobs
-              </div>
-            </Link>
-            <Link href={`/jobs/student-casual`}>
-              <div className="badge badge-outline p-3">Casual Academic Jobs</div>
-            </Link>
-            <Link href={`/jobs/student-full-time`}>
-              <div className="badge badge-outline p-3">
-                Full-Time Academic Jobs
-              </div>
-            </Link>
-            <Link href={`/student`}>
-              <div className="badge badge-outline p-3">
-                Find All Academic Jobs
-              </div>
-            </Link>
-          </div> */}
-          {/* Diverse Roles Card */}
-         
-         
-         
-         
-         
-          <div className="card card-side bg-slate-100 shadow-xl my-16 px-6 grid sm:grid-cols-1 md:grid-cols-2">
-            <figure>
-              <Image
-                width={2000}
-                height={534}
-                src="https://academicjobs.s3.amazonaws.com/img/banner/collaboration+over+competition+.jpg"
-                alt="Choose from diverse campus roles"
-                className="w-full lg:max-w-lg rounded-xl m-6"
-              />
-           
-            </figure>
-            
-            
-            
-            
-            <div className="card-body w-full">
-              <h2 className="card-title">
-              COLLABORATION OVER COMPETITION
-              </h2>
-              <p className="max-w-screen-sm">
-              By providing a rank for you, we can set a benchmark that encourages collaboration. Having a number in the academic field allows you to find others with a similar rank, acknowledging a similar skill set, academic history, and research expertise. This knowledge can encourage collaborations on academic projects, expanding both members knowledge set and creativity.
-              </p>
+       
+
+
+
+
+
         
-              <div className="card-actions justify-center md:justify-end">
-                <Link className="btn btn-aj" href="/student">
-                  Search Academic Rankings
-                </Link>
-              </div>
-            </div>
-          </div>
-          {/* Flexible Part-Time Opportunities for Academics Card */}
-          <div className="card card-side bg-sky-50 shadow-xl my-16 px-6 grid sm:grid-cols-1 md:grid-cols-2">
-            <figure className="lg:order-1">
-              <Image
-                width={2000}
-                height={534}
-                src="https://academicjobs.s3.amazonaws.com/img/banner/analyse+your+academic+progress.jpg"
-                alt="Work Part-Time with Flexible Opportunities for Academics"
-                className="w-full lg:max-w-lg rounded-xl m-6"
-              />
-            </figure>
-            <div className="card-body w-full">
-              <h2 className="card-title">
-              ANALYSE YOUR ACADEMIC PROGRESS
-              </h2>
-              <p className="max-w-screen-sm">
-              Academics are known to engage in large depths of analysis behind their journals, reports, and projects. A legitimate rank of the members of academia can allow for further analysis on the performance of modern-day academics and their improvement over the years. In turn, this analysis can describe positive trends on the future of academia and promote further success in the field.
-              </p>
-              <div className="card-actions justify-center md:justify-start">
-                <Link className="btn btn-aj" href="/student">
-                  Earn Extra Cash
-                </Link>
-              </div>
-            </div>
-          </div>
-        </section>
         {/* Breakout Section */}
         <div className="bg-sky-200 mb-8 mt-16 py-8 px-4">
           <div className="container mx-auto">
@@ -348,103 +405,9 @@ export default function myPage() {
             </div>
           </div>
         </div>
-        <section className="container mx-auto px-4 mt-12">
-          {/* Gain Real-World Experience in Academic Settings Card */}
-          <div className="card card-side bg-slate-100 shadow-xl my-16 px-6 grid sm:grid-cols-1 md:grid-cols-2">
-            <figure>
-              <Image
-                width={2000}
-                height={534}
-                src="/student-jobs-on-campus/real-world-experience.jpg"
-                alt="Experience the Real-World in Academic Settings"
-                className="w-full lg:max-w-lg rounded-xl m-6"
-              />
-            </figure>
-            <div className="card-body w-full">
-              <h2 className="card-title">
-                {/* Gain Real-World Experience in Academic Settings */}
-              </h2>
-              <p className="max-w-screen-sm">
-                {/* Step into roles that provide real-world experience right on a
-                college campus. Engage in jobs that enhance your academic
-                knowledge, such as research assistants or departmental aides.
-                These positions not only offer practical experience but also a
-                deeper understanding of your field of study. */}
-              </p>
-              <div className="card-actions justify-center md:justify-end">
-                <Link className="btn btn-aj" href="/student">
-                  {/* Gain Valuable Skills */}
-                </Link>
-              </div>
-            </div>
-          </div>
-
-          {/* Build Your Network with On-Campus Employment Card */}
-          <div className="card card-side bg-sky-50 shadow-xl my-16 px-6 grid sm:grid-cols-1 md:grid-cols-2">
-            <figure className="lg:order-1">
-              <Image
-                width={2000}
-                height={534}
-                src="/student-jobs-on-campus/networking-with-campus-jobs.jpg"
-                alt="Build Your Network with On-Campus Employment"
-                className="w-full lg:max-w-lg rounded-xl m-6"
-              />
-            </figure>
-            <div className="card-body w-full">
-              <h2 className="card-title">
-                {/* Build Your Network with On-Campus Employment */}
-              </h2>
-              <p className="max-w-screen-sm">
-                {/* Working on campus is more than just a job; it's an opportunity
-                to build your professional and academic network. Interact with
-                faculty, staff, and fellow students in roles that expand your
-                connections and open doors for future career opportunities. */}
-              </p>
-              <div className="card-actions justify-center md:justify-start">
-                <Link className="btn btn-aj" href="/student">
-                  {/* Develop Your Network */}
-                </Link>
-              </div>
-            </div>
-          </div>
-        </section>
-        <div className="bg-teal-100 mb-8 mt-16 py-8 px-4">
-          <div className="container mx-auto">
-            <h2 className="text-4xl font-bold mb-8 text-sky-600">
-              {/* Let Academic Jobs buy your next beer or help fund your
-              higher&nbsp;education… */}
-            </h2>
-            <div className="text-col-2">
-              <p className="mb-4">
-                {/* Unlock Your Potential with Campus Employment Opportunities from
-                Academic Jobs. Our platform is tailored to help students like
-                you find the ideal on-campus job that complements your academic
-                schedule. Academic Rankings can provide the cash you need
-                to go out, buy a new gadget or pay eduction expenses? */}
-              </p>
-              <p className="mb-4">
-                {/* With Academic Jobs, you get access to a wide array of
-                student-focused positions that not only provide a source of
-                income but also enhance your university experience. From library
-                aides to research assistants, we connect you to roles that align
-                with your study goals and personal interests. */}
-              </p>
-              {/* <blockquote>
-               The best thing a human being can do is to help another human being know more.
-              </blockquote> */}
-              <p className="mb-4">
-                {/* Maximize Your College Experience with Academic Jobs. We
-                understand the unique needs of Academiclife, and our service is
-                designed to simplify your job search on campus. Whether you're
-                looking to develop professional skills, build your network, or
-                manage your educational expenses, Academic Jobs is your go-to
-                resource. */}
-              </p>
-            </div>
-          </div>
-        </div>
-
-  <section>
+        
+      
+          <section>
           <h3 className="container mx-auto text-center py-8 px-4">
             Want opportunities delivered to you?{' '}
             <Link href="/academic-talent-pool" className="btn btn-aj">
