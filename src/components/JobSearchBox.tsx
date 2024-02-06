4deaac8f0694c5d6dbe08647503524b4b68bdac3
Dan Grant <dan@zygadox.com>
2024-02-07 10:29:48 +1100

    Fix nested button issue

--- src/components/JobSearchBox.tsx
+++ src/components/JobSearchBox.tsx
@@ -57,12 +57,7 @@ export default function JobSearchBox({ q, l, forceClass }: MyComponentProps) {
               type="submit"
               className="indicator md:w-fit w-full bg-amber-500 md:bg-0 justify-center items-center animate-pulse"
             >
-              <button
-                type="submit"
-                className="btn join-item bg-amber-500 md:border md:border-gray-300 md:shadow-md border-0 text-white animate-pulse "
-              >
-                Search
-              </button>
+              Search
             </button>
           </div>
         </form>
