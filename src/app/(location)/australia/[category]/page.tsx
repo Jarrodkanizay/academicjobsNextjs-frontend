47bfeedb53fdce374b530ab23de79fe299959890
Dan Grant <dan@zygadox.com>
2024-02-09 15:59:25 +1100

    Update HR Page

--- src/app/(location)/australia/[category]/page.tsx
+++ src/app/(location)/australia/[category]/page.tsx
@@ -78,12 +78,12 @@ export default function Page({ params, searchParams }: any) {
                 {para}
               </p>
             ))}
-             <Link
-                      href={'/academic-talent-pool'}
-                      className="btn btn-aj btn-sm w-1/2 md:w-1/4 mx-auto my-2"
-                    >
-                     Join Our Talent Pool
-                    </Link>
+            <Link
+              href={'/academic-talent-pool'}
+              className="btn btn-aj btn-sm w-1/2 md:w-1/4 mx-auto my-2"
+            >
+              Join Our Talent Pool
+            </Link>
           </div>
         </div>
       </div>
