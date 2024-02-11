8c81715db2f7302010428972c1a81545b822dc47
Dan Grant <dan@zygadox.com>
2024-02-12 09:27:48 +1100

    Fix form messages

--- src/components/forms/TalentPoolForm.js
+++ src/components/forms/TalentPoolForm.js
@@ -442,6 +442,7 @@ const TalentPoolForm = ({
           name="20_Talent_Pool_input"
           className="w-full textarea textarea-bordered focus:outline-none focus:border-orange-500"
           placeholder="OPTIONAL - Please write a brief profile noting your key skills, experience, and achievements."
+          {...register('20_Talent_Pool_input')}
         />
       </div>
       {/* End form inputs */}
