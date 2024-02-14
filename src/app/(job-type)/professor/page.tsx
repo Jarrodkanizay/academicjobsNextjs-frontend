f0c5734e16faf8480a2786e726df097edc8433e2
Dan Grant <dan@zygadox.com>
2024-02-14 13:08:44 +1100

    Add logo banner/filter to Research page

--- src/app/(job-type)/professor/page.tsx
+++ src/app/(job-type)/professor/page.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
-import SearchResults from '@/components/SearchResults'
-import JobSearchBox from '@/components/JobSearchBox'
-import professor from "@/data/professor.json";
+import SearchResults from '@/components/SearchResults';
+import JobSearchBox from '@/components/JobSearchBox';
+import professor from '@/data/professor.json';
 import Link from 'next/link';
 
 export const metadata: Metadata = {
@@ -16,43 +16,52 @@ export const metadata: Metadata = {
 export default function myPage() {
   return (
     <main className="content-grid">
-     
-
-
       <div className="bg-slate-200 full-width">
-      <div className="hero-content flex-col lg:flex-row mx-auto items-start py-12">
-    
-              <h1 className="md:text-6xl font-bold m-0 md:text-right text-gray-500 pb-4">Professor Jobs</h1> 
-              
-            <div className="">
-            
+        <div className="hero-content flex-col lg:flex-row mx-auto items-start py-12">
+          <h1 className="md:text-6xl font-bold m-0 md:text-right text-gray-500 pb-4">
+            Professor Jobs
+          </h1>
+
+          <div className="">
             <p className="px-7 mb-4 mt-1">
-            Discover your perfect professor position through the world’s best job board! At Academic Jobs we offer the most renowned professor jobs at the top universities. A career as an academic professor will provide you with the chance to engage with partners domestically and internationally, work with some of the brightest minds, and use top-notch facilities and resources.
+              Discover your perfect professor position through the world’s best
+              job board! At Academic Jobs we offer the most renowned professor
+              jobs at the top universities. A career as an academic professor
+              will provide you with the chance to engage with partners
+              domestically and internationally, work with some of the brightest
+              minds, and use top-notch facilities and resources.
             </p>
             <p className="px-7 md:flex hidden">
-            The No.1 job board for professors, Academic Jobs, provides an extensive list of opportunities for the future of your career, carefully sectioning the top professor jobs to find the best position for you!</p>
-            <p className="px-7 md:flex hidden">
-            Find all the best jobs here…</p>
+              The No.1 job board for professors, Academic Jobs, provides an
+              extensive list of opportunities for the future of your career,
+              carefully sectioning the top professor jobs to find the best
+              position for you!
+            </p>
+            <p className="px-7 md:flex hidden">Find all the best jobs here…</p>
             <Link
-                      href={'/academic-talent-pool'}
-                      className="btn btn-aj btn-sm w-2/3 md:w-1/4 mx-auto my-2"
-                    >
-                     Join Our Talent Pool
-                    </Link>
-         
-            </div>
+              href={'/academic-talent-pool'}
+              className="btn btn-aj btn-sm w-2/3 md:w-1/4 mx-auto my-2"
+            >
+              Join Our Talent Pool
+            </Link>
           </div>
-          </div>
+        </div>
+      </div>
 
-          <div className="bg-white flex flex-col  ">
-               <ul className=" text-left columns-1 md:columns-4 pt-4 pl-8 md:gap-2  h-auto w-full text-transform: capitalize">
-              {professor.map(({ Title, Name, }, key) => (
-                <li className={`pb-2 flex ${' md:break' } ${ ' font-bold text-gray-400' }`} key={key}>
-                  <Link href={`/professor/${Name?.replace(/\s+/g, '-')}/`}>{Name}</Link>
-                </li>
-              ))}
-            </ul>
-          </div>
+      <div className="bg-white flex flex-col  ">
+        <ul className=" text-left columns-1 md:columns-4 pt-4 pl-8 md:gap-2  h-auto w-full text-transform: capitalize">
+          {professor.map(({ Title, Name }, key) => (
+            <li
+              className={`pb-2 flex ${' md:break'} ${' font-bold text-gray-400'}`}
+              key={key}
+            >
+              <Link href={`/professor/${Name?.replace(/\s+/g, '-')}/`}>
+                {Name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
 
       <JobSearchBox q="professor" />
       <SearchResults q={{ q: 'professor' || 0 }} />
--- src/app/(job-type)/research/page.tsx
+++ src/app/(job-type)/research/page.tsx
@@ -3,8 +3,7 @@ import Link from 'next/link';
 import SearchResults from '@/components/SearchResults';
 import JobSearchBox from '@/components/JobSearchBox';
 import JobFilter from '@/components/JobFilter';
-import research from "@/data/research.json";
-
+import research from '@/data/research.json';
 
 export const metadata: Metadata = {
   // title: 'About', //Option 1 replaces the %s in layout.tsx
@@ -18,52 +17,74 @@ export const metadata: Metadata = {
 export default function myPage() {
   return (
     <main className="content-grid">
-     
-
       <div className="bg-slate-200 full-width">
-      <div className="hero-content flex-col lg:flex-row mx-auto items-start py-12">
-    
-              <h1 className="md:text-6xl font-bold m-0 md:text-right text-gray-500 pb-4">Research Jobs</h1> 
-              
-            <div className="">
-            
+        <div className="hero-content flex-col lg:flex-row mx-auto items-start py-12">
+          <h1 className="md:text-6xl font-bold m-0 md:text-right text-gray-500 pb-4">
+            Research Jobs
+          </h1>
+
+          <div className="">
             <p className="px-7 mb-4 mt-1">
-            Find your perfect research position through the world’s best job board! At Academic Jobs we offer the most renowned research jobs at the top universities. A career as an academic researcher will provide you with the chance to engage with partners domestically and internationally, work with some of the brightest minds, and use top-notch facilities and resources.
+              Find your perfect research position through the world’s best job
+              board! At Academic Jobs we offer the most renowned research jobs
+              at the top universities. A career as an academic researcher will
+              provide you with the chance to engage with partners domestically
+              and internationally, work with some of the brightest minds, and
+              use top-notch facilities and resources.
             </p>
             <p className="px-7 md:flex hidden">
-            The No.1 job board for researchers, Academic Job, provides an extensive list of opportunities for the future of your career, carefully sectioning the top research jobs to find the best position for you!
+              The No.1 job board for researchers, Academic Job, provides an
+              extensive list of opportunities for the future of your career,
+              carefully sectioning the top research jobs to find the best
+              position for you!
             </p>
             <p className="px-7 md:flex hidden">
-            Find all the best research jobs here…
+              Find all the best research jobs here…
             </p>
             <Link
-                      href={'/academic-talent-pool'}
-                      className="btn btn-aj btn-sm w-2/3 md:w-1/4 mx-auto my-2"
-                    >
-                     Join Our Talent Pool
-                    </Link>
-         
-            </div>
+              href={'/academic-talent-pool'}
+              className="btn btn-aj btn-sm w-2/3 md:w-1/4 mx-auto my-2"
+            >
+              Join Our Talent Pool
+            </Link>
           </div>
-          </div>
+        </div>
+      </div>
 
-          <div className="bg-white flex flex-col  ">
-               <ul className=" text-left columns-1 md:columns-4 pt-4 pl-8 md:gap-2  h-auto w-full text-transform: capitalize">
-              {research.map(({ Title, Name, }, key) => (
-                <li className={`pb-2 flex ${' md:break' } ${ ' font-bold text-gray-400' }`} key={key}>
-                  <Link href={`/research/${Name?.replace(/\s+/g, '-')}/`}>{Name}</Link>
-                </li>
-              ))}
-            </ul>
-          </div>
-
+      <div className="bg-white flex flex-col  ">
+        <ul className=" text-left columns-1 md:columns-4 pt-4 pl-8 md:gap-2  h-auto w-full text-transform: capitalize">
+          {research.map(({ Title, Name }, key) => (
+            <li
+              className={`pb-2 flex ${' md:break'} ${' font-bold text-gray-400'}`}
+              key={key}
+            >
+              <Link href={`/research/${Name?.replace(/\s+/g, '-')}/`}>
+                {Name}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
 
       <Link href="/research/faq" className="pt-4 text-sky-500">
         View Research Jobs FAQ
       </Link>
 
-      <JobSearchBox q= 'research' />
-      <SearchResults q={{ q: 'research' || 0 }} />
+      <JobSearchBox q="research" forceClass="my-6" />
+
+      <section class="jobs_grid job_post_search_container">
+        <div class="filters_panel">
+          <div class="filters_content">
+            <JobFilter />
+          </div>
+        </div>
+        <div class="listings_panel">
+          <div class="listings_content">
+            {/* <SearchResults1 q={{ q: q, l: l || '', filter1 }} /> */}
+            <SearchResults q={{ q: 'research' || 0 }} />
+          </div>
+        </div>
+      </section>
     </main>
   );
 }
