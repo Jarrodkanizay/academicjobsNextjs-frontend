08f0fb841018fbeb67eaced4c6f43f250e0ced70
SeanAcademicJobs <sean@academicjobs.com>
2024-02-29 16:00:42 +1100

    TalentPool CTA

---
+++ src/components/TalentPoolCTA.tsx
@@ -0,0 +1,28 @@
+// type FAQProps = { children: any; question: string };
+
+const TalentPool = ({ children, question }: TalentPoolProps) => {
+  return (
+    <div className="alert mt-8">
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      className="stroke-info shrink-0 w-6 h-6"
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+        d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+      ></path>
+    </svg>
+    <span className=" text-sky-500 font-bold">Join the Boston Talent Pool</span>
+    <div>
+      
+      <a className="btn-aj text-white p-2 px-4 font-bold" href="/academic-talent-pool">Join Now</a>
+    </div>
+  </div>
+  );
+};
+
+export default TalentPool;
