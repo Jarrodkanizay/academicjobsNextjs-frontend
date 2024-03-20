279a3c987aa62b5e746fafefbdc811ffd04a3af9
DanZmanGrant <dan@zygadox.com>
2024-03-21 07:17:57 +1100

    Fix URL

--- src/app/(wip)/auth-dev/signin/SignInForm.tsx
+++ src/app/(wip)/auth-dev/signin/SignInForm.tsx
@@ -109,7 +109,10 @@ const SignInForm = (props: Props) => {
           </GoogleSignInButton>
           <p className="text-center text-sm text-gray-600 mt-2">
             If you don&apos;t have an account, please&nbsp;
-            <Link className="text-blue-500 hover:underline" href="/auth/signup">
+            <Link
+              className="text-blue-500 hover:underline"
+              href="/auth-dev/signup"
+            >
               Sign up
             </Link>
           </p>
