e57a7f5cbb7b7a941707550f77f3aa5fbb217034
DanZmanGrant <dan@zygadox.com>
2024-03-21 08:21:07 +1100

    Add profile banner

--- src/app/(users)/my_life/page.js
+++ src/app/(users)/my_life/page.js
@@ -10,7 +10,8 @@ import { filterType } from '@/utils/data';
 import Link from 'next/link';
 import { keepPreviousData, useQuery } from '@tanstack/react-query';
 import BaseApi from '@/lib/store/Base';
-import {  useSession } from 'next-auth/react';
+import { useSession } from 'next-auth/react';
+import ProfileBanner from './ProfileBanner';
 
 export default function Page() {
   const { data: session } = useSession();
@@ -40,31 +41,27 @@ export default function Page() {
   //   enabled: category !== '',
   // });
 
-
   return (
-   
-      <main>
-        <div className="full-width md:h-[120px] h-[120px] gradient-aj">
-     
-     <section className="wrapper md:flex">
-       <h2 className="sentence ">
-         <span className="mr-8">Find</span>{' '}
-         <span className="md:hidden block">
-           <br />
-         </span>
-         <div className="slidingVertical ">
-           <span>Opportunity</span>
-           <span>Connections</span>
-           <span>Happiness</span>
-           <span>Opportunity</span>
-           <span>Connections</span>
-         </div>
-       </h2>
-     </section>
-     </div>
-     <h1 className="max-w-5xl mx-auto text-center">My Life, My Profile</h1>
-      <div className=" mx-auto max-w-5xl  flex flex-col  ">
-        
+    <main>
+      <div className="full-width md:h-[120px] h-[120px] gradient-aj">
+        <section className="wrapper md:flex">
+          <h2 className="sentence ">
+            <span className="mr-8">Find</span>{' '}
+            <span className="md:hidden block">
+              <br />
+            </span>
+            <div className="slidingVertical ">
+              <span>Opportunity</span>
+              <span>Connections</span>
+              <span>Happiness</span>
+              <span>Opportunity</span>
+              <span>Connections</span>
+            </div>
+          </h2>
+        </section>
+      </div>
+      <section title="Profile" className="content-grid">
+        <ProfileBanner />
         {/* {
          
           filter1.length > 0 && (
@@ -90,8 +87,7 @@ export default function Page() {
             </div>
           )
         } */}
-      </div>
-      </main>
-   
+      </section>
+    </main>
   );
 }
