e3bc48ced4ea75a74cba658d9542f07c968576a4
SeanAcademicJobs <sean@academicjobs.com>
2024-03-20 16:41:52 +1100

    sign up pages

--- src/app/(users)/my_life/page.js
+++ src/app/(users)/my_life/page.js
@@ -44,7 +44,27 @@ export default function Page() {
   return (
    
       <main>
+        <div className="full-width md:h-[120px] h-[120px] gradient-aj">
+     
+     <section className="wrapper md:flex">
+       <h2 className="sentence ">
+         <span className="mr-8">Find</span>{' '}
+         <span className="md:hidden block">
+           <br />
+         </span>
+         <div className="slidingVertical ">
+           <span>Opportunity</span>
+           <span>Connections</span>
+           <span>Happiness</span>
+           <span>Opportunity</span>
+           <span>Connections</span>
+         </div>
+       </h2>
+     </section>
+     </div>
+     <h1 className="max-w-5xl mx-auto text-center">My Life, My Profile</h1>
       <div className=" mx-auto max-w-5xl  flex flex-col  ">
+        
         {/* {
          
           filter1.length > 0 && (
