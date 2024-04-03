25379bf3a794144d66167532fdc384fea5f977c8
WalterJin2 <walterjin2@gmail.com>
2024-04-03 14:29:39 +1100

    1

--- src/app/(website)/specialty-jobs/[slug]/page.tsx
+++ src/app/(website)/specialty-jobs/[slug]/page.tsx
@@ -204,13 +204,13 @@ export default function BlogPostPage({ params }: Props) {
             /> */}
             <SearchResults1
               q={{
-                filter1: [
-                  {
-                    category: specialtyDetails.jobCategory,
-                    filter: '',
-                  },
-                ],
-                q: '',
+                // filter1: [
+                //   {
+                //     category: specialtyDetails.jobCategory,
+                //     filter: '',
+                //   },
+                // ],
+                q: 'physics',
                 l: '',
               }}
             />
