7c912bb27e6a28faf2540bfd3093131f00efccff
Cian Fitzgerald <166671267+CianAJ@users.noreply.github.com>
2024-04-11 16:06:02 +1000

    changes
    - updated faculty page with grid
    - removed student faq page
    - moved job fulfilled stamp

--- src/app/(website)/contact-us/page.tsx
+++ src/app/(website)/contact-us/page.tsx
@@ -31,6 +31,65 @@ export default function myPage() {
           </h4>
         </div>
 
+        
+        <div className="flex flex-col lg:flex-row lg:gap-8">
+          <ContactUsForm maxWidth="700" />
+          {/* Contact Numbers */}
+          <div className="flex flex-col w-full bg-slate-100 py-4 px-8 rounded-2xl lg:min-w-[450px] lg:max-w-[500px] mt-8 lg:mt-0">
+            <h2 className="underline-full mb-6">Global Office Numbers</h2>
+            <ul className="contact-number mb-4">
+              {/* <li>
+                <a className="footer-nav__link" href="tel:+852-3730021">
+                  <strong>Asia: </strong>+852-3730021 (Hong Kong)
+                </a>
+              </li> */}
+              <li>
+                <a className="footer-nav__link" href="tel:+61-452070156">
+                  <strong>Australia: </strong> +61-452070156 (Melbourne)
+                </a>
+              </li>
+              <li>
+                <a className="footer-nav__link" href="tel:+61-452070156">
+                  <strong>New Zealand: </strong>+61-452070156 (Wellington)
+                </a>
+              </li>
+
+              {/* <li>
+                <a className="footer-nav__link" href="tel:778 819 1866">
+                  <strong>Canada: </strong>778 819 1866 (Vancouver)
+                </a>
+              </li>
+              <li>
+                <a className="footer-nav__link" href="tel:+415 819 0021">
+                  <strong>United States: </strong>415 819 0021 (San Francisco)
+                </a>
+              </li>
+              <li>
+                <a className="footer-nav__link" href="tel:+49-4158730021">
+                  <strong>Europe: </strong>+49-4158730021 (Berlin)
+                </a>
+              </li>
+              <li>
+                <a className="footer-nav__link" href="tel:+91-2250972736">
+                  <strong>India: </strong>+91-2250972736 (Mumbai)
+                </a>
+              </li>
+              <li>
+                <a className="footer-nav__link" href="tel:+44-2045876329">
+                  <strong>United Kingdom: </strong>+44-2045876329 (London)
+                </a>
+              </li> */}
+            </ul>
+            <Image
+              src="/contact-us/global-office-numbers.jpg"
+              width={500}
+              height={500}
+              alt="Global Office Numbers"
+              className="rounded-xl mx-auto mt-auto mb-4"
+            />
+          </div>
+        </div>
+
         <div className="container mt-16">
           <div className="stats shadow w-full">
             <div className="stat">
@@ -151,64 +210,6 @@ export default function myPage() {
                 </Link>
               </div>
             </div>
-          </div>
-        </div>
-
-        <div className="flex flex-col lg:flex-row lg:gap-8">
-          <ContactUsForm maxWidth="700" />
-          {/* Contact Numbers */}
-          <div className="flex flex-col w-full bg-slate-100 py-4 px-8 rounded-2xl lg:min-w-[450px] lg:max-w-[500px] mt-8 lg:mt-0">
-            <h2 className="underline-full mb-6">Global Office Numbers</h2>
-            <ul className="contact-number mb-4">
-              {/* <li>
-                <a className="footer-nav__link" href="tel:+852-3730021">
-                  <strong>Asia: </strong>+852-3730021 (Hong Kong)
-                </a>
-              </li> */}
-              <li>
-                <a className="footer-nav__link" href="tel:+61-452070156">
-                  <strong>Australia: </strong> +61-452070156 (Melbourne)
-                </a>
-              </li>
-              <li>
-                <a className="footer-nav__link" href="tel:+61-452070156">
-                  <strong>New Zealand: </strong>+61-452070156 (Wellington)
-                </a>
-              </li>
-
-              {/* <li>
-                <a className="footer-nav__link" href="tel:778 819 1866">
-                  <strong>Canada: </strong>778 819 1866 (Vancouver)
-                </a>
-              </li>
-              <li>
-                <a className="footer-nav__link" href="tel:+415 819 0021">
-                  <strong>United States: </strong>415 819 0021 (San Francisco)
-                </a>
-              </li>
-              <li>
-                <a className="footer-nav__link" href="tel:+49-4158730021">
-                  <strong>Europe: </strong>+49-4158730021 (Berlin)
-                </a>
-              </li>
-              <li>
-                <a className="footer-nav__link" href="tel:+91-2250972736">
-                  <strong>India: </strong>+91-2250972736 (Mumbai)
-                </a>
-              </li>
-              <li>
-                <a className="footer-nav__link" href="tel:+44-2045876329">
-                  <strong>United Kingdom: </strong>+44-2045876329 (London)
-                </a>
-              </li> */}
-            </ul>
-            <Image
-              src="/contact-us/global-office-numbers.jpg"
-              width={500}
-              height={500}
-              alt="Global Office Numbers"
-              className="rounded-xl mx-auto mt-auto mb-4"
-            />
           </div>
         </div>
       </main>
