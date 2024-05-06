5da9ba761db0cff43300cab0a253d0085cfd6d34
JamieRuskin <166671267+CianAJ@users.noreply.github.com>
2024-05-06 17:12:36 +1000

    fixing title

--- src/app/(jobs)/jobs/page.js
+++ src/app/(jobs)/jobs/page.js
@@ -2,7 +2,7 @@ import Jobs from "./jobs";
 export async function generateMetadata({ params, searchParams }) {
 
     return {
-        title: `${searchParams.l != "" || searchParams.q != "" ? searchParams.l + ' ' + searchParams.q : ""}`,
+        title: `${searchParams.l != undefined || searchParams.q != undefined ? searchParams.l + ' ' + searchParams.q + "|": ""}`,
     };
 }
 export default function Page({ searchParams }) {
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ const inter = Inter({ subsets: ['latin'] });
 export const metadata: Metadata = {
   title: {
     default: 'The Best Academic Jobs', //Default if nothing is set
-    template: '%s | Academic Jobs', //If the title is set on the page the %s will be replaced with the title
+    template: '%s Academic Jobs', //If the title is set on the page the %s will be replaced with the title
   },
   description: "Academic Jobs the worlds' number 1 job board",
   icons: {
