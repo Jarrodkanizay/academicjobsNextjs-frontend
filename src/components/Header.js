b8a7977f07d34c4b2e87ccb6f1d7d534bd8d2203
WalterJin2 <walterjin2@gmail.com>
2024-05-08 14:42:49 +1000

    q

--- src/components/Header.js
+++ src/components/Header.js
@@ -129,7 +129,7 @@ export default function Header() {
           <NavItem
             url={`/${countryMappings2[region.toLowerCase()].url}/jobs`}
             icon="/dotted-arrow.svg"
-            navLink="Seek1 Jobs"
+            navLink="Seek Jobs"
             forceClass="border-b hover:border-amber-500" //this is optional
             forceButtonClass="border-b hover:border-amber-500 mr-2 "
             onClick={() => setIsNavOpen(false)}
