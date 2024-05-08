cb72a3e65773d853d9a92ab3ae9061b04cc2e7ec
WalterJin2 <walterjin2@gmail.com>
2024-05-08 14:30:40 +1000

    1

--- src/lib/store/Base/index.tsx
+++ src/lib/store/Base/index.tsx
@@ -1,10 +1,10 @@
 import axios from 'axios';
 
 // alert(process.env.NEXT_PUBLIC_DEPLOY_MODE);
-// console.log(
-//   'process.env.NEXT_PUBLIC_DEPLOY_MODE=========================',
-//   process.env.NEXT_PUBLIC_DEPLOY_MODE
-// );
+ console.log(
+   'process.env.NEXT_PUBLIC_DEPLOY_MODE=========================',
+  process.env.NEXT_PUBLIC_DEPLOY_MODE
+ );
 
 export const baseURL = 'https://api2.sciencejobs.com.au/api';
   // process.env.NEXT_PUBLIC_DEPLOY_MODE === 'development'
