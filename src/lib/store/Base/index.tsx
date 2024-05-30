52b15e2d737094901033ea53a9d604ceea5ec2fb
WalterJin2 <walterjin2@gmail.com>
2024-05-31 09:18:37 +1000

    1

--- src/lib/store/Base/index.tsx
+++ src/lib/store/Base/index.tsx
@@ -7,7 +7,7 @@ import axios from 'axios';
  );
 
 export const baseURL = 
- 'https://api2.sciencejobs.com.au/api';//'http://localhost:3500/api'// 'https://api2.sciencejobs.com.au/api'; // //'https://api2.sciencejobs.com.au/api' //
+ 'https://api2.sciencejobs.com.au/api';
 
 export const BaseApi = axios.create({
   baseURL,
