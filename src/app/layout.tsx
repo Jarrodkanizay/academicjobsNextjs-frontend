61d4bd889b46cb6bae43714f0f44bd79e81f088e
CianAJ <166671267+CianAJ@users.noreply.github.com>
2024-06-11 14:00:04 +1000

    removing comp pop up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,7 +39,7 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={`${inter.className} bg-white`}>
-        <CompetitionForm />
+        {/* <CompetitionForm /> */}
         <QueryProvider>
           <Providers session={session}>
             <Header />
