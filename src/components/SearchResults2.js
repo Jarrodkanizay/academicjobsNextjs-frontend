02f6c229827fbe28ed732d69310c305b2e352a97
WalterJin2 <walterjin2@gmail.com>
2024-06-17 15:35:26 +1000

    1

--- src/components/SearchResults2.js
+++ src/components/SearchResults2.js
@@ -22,7 +22,7 @@ export default function SearchResults2({
   const [page, setPage] = useState(0);
   //console.log('filter1', q, page);
   //useGetQtyQuery({ q: q, l: l || '', filter1 });
-  const { r = "", q = "", l = "", lon = 0, lat = 0, filter0 = [], currentMiddleCategory, category } = searchParams
+  const { r = "", q = "", l = "", lon = 0, lat = 0, filter0 = [], currentMiddleCategory, category } = useURLParams()
   let filter1 = [...filter0]
   const {
     isPending,
