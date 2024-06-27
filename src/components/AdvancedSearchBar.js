04b3b98a13a325ab2d580330fc47ffc93bcaf4aa
WalterJin2 <walterjin2@gmail.com>
2024-06-27 16:35:29 +1000

    去

--- src/components/AdvancedSearchBar.js
+++ src/components/AdvancedSearchBar.js
@@ -65,7 +65,7 @@ export default function Page({ p = {}, forceClass = '' }) {
   const [filter2, setfilter2] = useState([]);
   //alert(r)
   useEffect(() => {
-    ///////alert(category)
+    ///////////alert(category)
     //setCurrentMiddleCategory('')
     //setSearchJobQuery({ filter1 })
     setfilterTypes1((p) => ({ ...p, ExecutiveJobs: false }));
