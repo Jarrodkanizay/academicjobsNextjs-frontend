4af2fed9c9f373c3ec7dd9f436f8994900eaf028
WalterJin2 <walterjin2@gmail.com>
2024-07-03 15:03:24 +1000

    q

--- src/components/JobSearchBoxCountry.tsx
+++ src/components/JobSearchBoxCountry.tsx
@@ -56,7 +56,8 @@ export default function JobSearchBoxCountry({ country }) {
      //router.push(`/jobs-advanced-search?r=${r}`);
     router.push(
       `/jobs-advanced-search?${toURLParams({
-        ...searchParams, r:country,
+        ...searchParams,
+        r: countryMap[country],
         q: keyWordRef.current.value.trim(),
       })}`,
       { scroll: false }
