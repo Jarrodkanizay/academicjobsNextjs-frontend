eff35bbe38730974e9578df95f11212c9c895f55
DanZmanGrant <dan@zygadox.com>
2024-07-25 13:44:20 +1000

    Remove test message

--- src/components/LocalJobLinks.tsx
+++ src/components/LocalJobLinks.tsx
@@ -25,7 +25,7 @@ const LocalJobLinks = ({
     <>
       {category.toLowerCase() === 'indigenous' ? null : (
         <div className="links_section mb-16">
-          <p>{category}</p>
+          {/* <p>{category}</p> */}
           <h2>{heading}</h2>
           <nav aria-label={heading}>
             <ul>
