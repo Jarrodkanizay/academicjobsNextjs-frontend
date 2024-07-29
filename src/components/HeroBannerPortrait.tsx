b0049ae1ae5b1ec566d4ad9fd94894c1c93eaef8
DanZmanGrant <dan@zygadox.com>
2024-07-29 12:30:04 +1000

    Add back animated text

--- src/components/HeroBannerPortrait.tsx
+++ src/components/HeroBannerPortrait.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import WordSwitcher from '@/components/WordSwitcher';
 
 type Props = {
   h1: string;
@@ -17,35 +18,30 @@ const HeroBannerPortrait = ({
   size = 180,
 }: Props) => {
   return (
-    <div className={`full-width h-[260px] md:h-[340px] gradient-aj`}>
-      <div className={`hero-bg ${country} h-full md:bg-center mb-4`}>
-        <section className="wrapper flex flex-col items-center justify-center">
-          <h1 className="sentence text-white text-center m-0 p-0">{h1}</h1>
-          {/* <h1 className="sentence text-white text-center m-0 p-0 md:hidden">
-            {h1_mobile}
-          </h1> */}
-        </section>
-
-        {/* Desktop Version */}
-        <ul className="hero-icons hide flex gap-1 md:gap-4 mx-auto text-center items-center justify-between md:justify-center mt-[-2rem] p-4">
-          {items.map((item, index) => (
-            <li key={index}>
-              <Link className="grayscale hover:grayscale-0" href={item.link}>
-                <Image
-                  src={item.src}
-                  width={size}
-                  height={size}
-                  alt={item.alt}
-                />
-              </Link>
-              <Link className="mb-4" href={item.link}>
-                <p>{item.label}</p>
-              </Link>
-            </li>
-          ))}
-        </ul>
+    <>
+      <div className={`full-width h-[260px] md:h-[340px] gradient-aj`}>
+        <div
+          className={`hero-bg ${country} h-full md:bg-center mb-4 flex items-center justify-center`}
+        >
+          <section className="wrapper flex flex-col items-center justify-center mt-[0.25rem] md:mt-[-5rem]">
+            <h1 className="sentence text-white text-center m-0 p-0">{h1}</h1>
+            <WordSwitcher />
+          </section>
+        </div>
       </div>
-    </div>
+      <ul className="hero-icons hide flex gap-1 md:gap-4 mx-auto text-center items-center justify-between md:justify-center mt-[-5rem] p-4">
+        {items.map((item, index) => (
+          <li key={index}>
+            <Link className="grayscale hover:grayscale-0" href={item.link}>
+              <Image src={item.src} width={size} height={size} alt={item.alt} />
+            </Link>
+            <Link className="mb-4" href={item.link}>
+              <p>{item.label}</p>
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
   );
 };
 
